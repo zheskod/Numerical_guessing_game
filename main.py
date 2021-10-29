3c09c5cdedca00db20c80bdc4368291102d7dae7
Alik <90277937+zheskod@users.noreply.github.com>
2021-10-30 01:09:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,27 @@ def is_valid(input_number):
             flag = True
     return flag
 
+left = 1
+right = 100
 correct_number = False
+
 while correct_number == False:
     user_number = input('Введите число от 1 до 100 (включительно): ')
     is_valid(user_number)  # Отправляем введённое число на проверку в функцию
     if is_valid(user_number) == True:  # Если проверку прошла
-        correct_number = True  # стопаем цикл while (меняем флаг на тру)
         user_number = int(user_number)  # преобразуем в целочисленное
+        user_name = (left + right) // 2
+        if user_number < num:
+            print('Ваше число меньше загаданного, попробуйте еще разок')
+            continue
+        elif user_number > num:
+            print('Ваше число больше загаданного, попробуйте еще разок')
+            continue
+        else:
+            correct_number = True
+            print('Вы угадали, поздравляем!')
     else:
         print('А может быть все-таки введем целое число от 1 до 100?')
         continue  # если не прошел условий функций, отправляем еще запрос
+
+print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
